706bda44ced9cf24ace132b298fe5367174879b3
David Mendoza <d.mendoza99@unitec.edu>
2020-07-19 13:09:20 -0600

    I think it's ready to start coding I might start with the auth package

---
+++ index.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const body = require('body-parser');
+const path = require('path');
+const fs = require('fs');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(cors());
+app.use(body.urlencoded({ extended: true }));
+app.use(body.json());
+app.use(helmet());
+
+if (!fs.existsSync(path.join(__dirname, 'logs'))) {
+  fs.mkdirSync(path.join(__dirname, 'logs'));
+}
+const date = new Date();
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, 'logs', `${date.toLocaleDateString().replace(/\//g, '-')}.log`),
+  { flags: 'a' },
+);
+app.use(morgan('tiny', { stream: accessLogStream }));
+
+app.listen(PORT, () => {
+  console.log(`App running at ${PORT}`);
+});
