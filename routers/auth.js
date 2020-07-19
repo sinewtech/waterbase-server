ff2d93fd94650df986b561681b49eeb6aa682b78
David Mendoza <d.mendoza99@unitec.edu>
2020-07-19 17:31:01 -0600

    simple CRUD it needs JWT and encryption

--- routers/auth.js
+++ routers/auth.js
@@ -1,20 +1,66 @@
 const express = require('express');
-// const mongoUtil = require('../helpers/mongoUtil');
+const Users = require('../models/Users');
 
 const Auth = express.Router();
 // create one user
-Auth.post('/', (req, res) => {});
+Auth.post('/', (req, res) => {
+  const { body } = req;
+  Users.create({ ...body })
+    .then((info) => {
+      res.status(200).json({ success: true, ...info });
+    })
+    .catch((error) => {
+      res.status(500).json({ success: false, ...error });
+    });
+});
 
 // update one user
-Auth.put('/update', (req, res) => {});
+Auth.put('/', (req, res) => {
+  const { body } = req;
+  const { where, document } = body;
+  Users.updateOne(where, { $set: document })
+    .then((data) => {
+      res.status(200).json({ success: true, info: data });
+    })
+    .catch((error) => {
+      res.status(500).json({ success: false, ...error });
+    });
+});
 
 // get all users
-Auth.get('/', (req, res) => {});
+Auth.get('/', (req, res) => {
+  Users.find()
+    .then((data) => {
+      res.status(200).json({ success: true, users: data });
+    })
+    .catch((error) => {
+      res.status(500).json({ success: false, ...error });
+    });
+});
 
 // get one user
-Auth.get('/:id', (req, res) => {});
+Auth.get('/:id', (req, res) => {
+  const { id } = req.params;
+  Users.findOne({ _id: id })
+    .then((data) => {
+      res.status(200).json({ success: true, user: data });
+    })
+    .catch((error) => {
+      res.status(500).json({ success: false, ...error });
+    });
+});
 
 // delete one user
-Auth.delete('/delete', (req, res) => {});
+Auth.delete('/', (req, res) => {
+  const { body } = req;
+  const { where } = body;
+  Users.deleteOne(where)
+    .then((data) => {
+      res.status(200).json({ success: true, info: data });
+    })
+    .catch((error) => {
+      res.status(500).json({ success: false, ...error });
+    });
+});
 
 module.exports = Auth;
