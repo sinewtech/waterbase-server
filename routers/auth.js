565ffd3416a66ed65b9bc4ff52328f8a3f5ec351
David Mendoza <d.mendoza99@unitec.edu>
2020-10-22 23:01:39 -0600

    made a lot of changes now the create and update take in mind the password

--- routers/auth.js
+++ routers/auth.js
@@ -1,57 +1,87 @@
 const express = require('express');
+const { hash } = require('bcryptjs');
 const Users = require('../models/Users');
+const defaultError = require('../middlewares/defaultError');
 
 const Auth = express.Router();
+const SALT = parseInt(process.env.SALT_ROUNDS, 10) || 10;
+
 // create one user
-Auth.post('/', (req, res) => {
+Auth.post('/', (req, res, next) => {
   const { body } = req;
-  Users.create({ ...body })
-    .then((info) => {
-      res.status(200).json({ success: true, ...info });
+  const { email, profile, password } = body;
+  hash(password, SALT)
+    .then((value) => {
+      Users.create({ email, profile: profile || {}, password: value })
+        .then((info) => {
+          res.status(200).json({ success: true, ...info });
+        })
+        .catch((error) => {
+          next(error);
+        });
     })
     .catch((error) => {
-      res.status(500).json({ success: false, ...error });
+      next(error);
     });
 });
 
 // update one user
-Auth.put('/', (req, res) => {
+Auth.put('/', (req, res, next) => {
   const { body } = req;
   const { where, document } = body;
-  Users.updateOne(where, { $set: document })
-    .then((data) => {
-      res.status(200).json({ success: true, info: data });
-    })
-    .catch((error) => {
-      res.status(500).json({ success: false, ...error });
-    });
+  Users.findOne(where, (err) => {
+    if (err) next(err);
+    if (!document.password) {
+      Users.updateOne(where, { $set: document })
+        .then((data) => {
+          res.status(200).json({ success: true, info: data });
+        })
+        .catch((error) => {
+          next(error);
+        });
+    } else {
+      hash(document.password, SALT)
+        .then((value) => {
+          Users.updateOne(where, { $set: { ...document, password: value } })
+            .then((info) => {
+              res.status(200).json({ success: true, ...info });
+            })
+            .catch((error) => {
+              next(error);
+            });
+        })
+        .catch((error) => {
+          next(error);
+        });
+    }
+  });
 });
 
 // get all users
-Auth.get('/', (req, res) => {
+Auth.get('/', (req, res, next) => {
   Users.find()
     .then((data) => {
       res.status(200).json({ success: true, users: data });
     })
     .catch((error) => {
-      res.status(500).json({ success: false, ...error });
+      next(error);
     });
 });
 
 // get one user
-Auth.get('/:id', (req, res) => {
+Auth.get('/:id', (req, res, next) => {
   const { id } = req.params;
   Users.findOne({ _id: id })
     .then((data) => {
       res.status(200).json({ success: true, user: data });
     })
     .catch((error) => {
-      res.status(500).json({ success: false, ...error });
+      next(error);
     });
 });
 
 // delete one user
-Auth.delete('/', (req, res) => {
+Auth.delete('/', (req, res, next) => {
   const { body } = req;
   const { where } = body;
   Users.deleteOne(where)
@@ -59,8 +89,10 @@ Auth.delete('/', (req, res) => {
       res.status(200).json({ success: true, info: data });
     })
     .catch((error) => {
-      res.status(500).json({ success: false, ...error });
+      next(error);
     });
 });
+
+Auth.use(defaultError);
 
 module.exports = Auth;
