595d48ab5078c9305b4a64b253e6f22551b29a52
David Mendoza <d.mendoza99@unitec.edu>
2020-10-23 18:20:00 -0600

    removed warning

--- helpers/mongoUtil.js
+++ helpers/mongoUtil.js
@@ -2,19 +2,19 @@ const mongoose = require('mongoose');
 
 function initDB() {
   const url = `${process.env.MONGO_URI}/${process.env.MONGO_DB}`;
-  mongoose
-    .connect(
-      url,
-      {
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-        user: process.env.MONGO_USER,
-        pass: process.env.MONGO_PASS,
-      },
-      (err) => {
-        if (err) throw err;
-      },
-    );
+  mongoose.connect(
+    url,
+    {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useFindAndModify: true,
+      user: process.env.MONGO_USER,
+      pass: process.env.MONGO_PASS,
+    },
+    (err) => {
+      if (err) throw err;
+    },
+  );
 }
 const { connection } = mongoose;
 
