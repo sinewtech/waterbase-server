1aac670e033430d9d8531508554325038e100dbb
David Mendoza <d.mendoza99@unitec.edu>
2020-10-23 18:22:36 -0600

    changed to files...

--- models/Medias.model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const mediaSchema = new mongoose.Schema({
-  url: { type: String, index: true, required: true },
-  name: { type: String, required: true },
-});
-
-module.exports = mongoose.model('waterbase_medias', mediaSchema);
--- routers/media.router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const defaultError = require('../middlewares/defaultError');
-
-const Media = express.Router();
-
-Media.use(defaultError);
-
-module.exports = Media;
