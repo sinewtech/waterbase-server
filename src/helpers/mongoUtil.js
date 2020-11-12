ef190224d2fe746b47f0fafdfc37269b944a8f72
David Mendoza <d.mendoza99@unitec.edu>
2020-11-11 22:35:55 -0600

    had bad the uri

--- src/helpers/mongoUtil.js
+++ src/helpers/mongoUtil.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 function initDB() {
-  const url = `${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_URI}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`;
+  const url = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_URI}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`;
   mongoose.connect(
     url,
     {
